b7ffd88a674c3d00eb2c9475f8dd920097c78281
Kyra Christensen <christensen.kyral@gmail.com>
2022-03-18 09:51:37 -0700

    setup boardGames db

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,17 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS boardGames;
+
+CREATE TABLE boardGames (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  game_name TEXT NOT NULL,
+  year_released INT,
+  num_of_players TEXT,
+  game_description TEXT 
+);
+
+INSERT INTO 
+  boardGames (game_name, year_released, num_of_players, game_description)
+VALUES
+  ('Sorry', 1929, '2-4', 'Players move their three or four pieces around the board, attempting to get all of their pieces "home" before any other player.');
