d434656a4d9a2fe1ec80c69ae5b26edd216ba001
Kyra Christensen <christensen.kyral@gmail.com>
2022-03-18 10:19:35 -0700

    created first test that should create a board game, created boardGames controller file

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,4 +11,17 @@ describe('hand-of-resources routes', () => {
   afterAll(() => {
     pool.end();
   });
+
+
+  it('creates a board game', async () => {
+    const expected = {
+      game_name: 'Life',
+      year_released: 1960,
+      num_of_players: '2-6',
+      description: 'simulates a person\'s travels through his or her life, from college to retirement, with jobs, marriage, and possible children along the way.'
+    };
+    const res = await request(app).post('/api/v1/boardGames').send(expected);
+
+    expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
 });
