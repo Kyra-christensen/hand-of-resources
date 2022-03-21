665446fda620971105e45663017e55fac25f7df7
Kyra Christensen <christensen.kyral@gmail.com>
2022-03-21 03:05:05 -0700

    typed test into wrong file, but it's fixed

---
+++ __tests__/characters.test.js
@@ -0,0 +1,26 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('hand-of-resources routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+
+  it ('creates a character', async () => {
+    const expected = {
+      character_name: 'Tanjiro Kamado',
+      age: 13,
+      from_anime: 'Demon Slayer',
+    };
+    const res = await request(app).post('/api/v1/characters').send(expected);
+
+    expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+});
