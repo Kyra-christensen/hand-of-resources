20da19039f5f4532d086c16912a3882459b56d07
Kyra Christensen <christensen.kyral@gmail.com>
2022-03-21 02:19:11 -0700

    created get all movies test

--- __tests__/movie.test.js
+++ __tests__/movie.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Movie = require('../lib/models/Movie');
 
 describe('hand-of-resources routes', () => {
   beforeEach(() => {
@@ -21,5 +22,11 @@ describe('hand-of-resources routes', () => {
     const res = await request(app).post('/api/v1/movies').send(expected);
 
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it ('gets a list of movies', async () => {
+    const expected = await Movie.getAll();
+    const res = await request(app).get('/api/v1/movies');
+    expect(res.body).toEqual(expected);
   });
 });
